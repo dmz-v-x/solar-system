9183b673f523440462a7ae03900ac91f6932a3d8
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2025-02-20 16:16:36 +0530

    updated app.js

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
     pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000,
+    socketTimeoutMS: 45000
 }, function(err) {
     if (err) {
         console.log("error!! " + err)
