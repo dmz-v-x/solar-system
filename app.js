724696d50f136f1159097f54139825d96fb0a9a5
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2025-02-20 16:37:50 +0530

    updated workflow yaml file

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ app.use(cors())
 mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
     pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     serverSelectionTimeoutMS: 30000,
     socketTimeoutMS: 45000
 })
